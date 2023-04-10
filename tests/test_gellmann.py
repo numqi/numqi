6bd645a86114f5276d7f6148b8b7aa773606f254
Sunny ZHU <zxrsunny613@gmail.com>
2023-04-10 14:45:30 +0800

    merge two test modules with the same name

--- tests/test_gellmann.py
+++ tests/test_gellmann.py
@@ -12,7 +12,10 @@ def test_matrix_to_gellman_basis():
         tmp0 = numpyqi.gellmann.all_gellmann_matrix(N0)
         ret0 = sum(x*y for x,y in zip(coeff,tmp0))
         assert np.abs(np0-ret0).max()<1e-7
-
+        for norm_I in ['1/d', 'sqrt(2/d)']:
+            np1 = numpyqi.gellmann.matrix_to_gellmann_basis(np0, norm_I=norm_I)
+            np2 = numpyqi.gellmann.gellmann_basis_to_matrix(np1, norm_I=norm_I)
+            assert np.abs(np0-np2).max() < 1e-7
 
 def test_all_gellmann_matrix():
     # https://arxiv.org/abs/1705.01523
@@ -21,16 +24,6 @@ def test_all_gellmann_matrix():
         for ind0,x in enumerate(all_term):
             for ind1,y in enumerate(all_term):
                 assert abs(np.trace(x @ y)-2*(ind0==ind1)) < 1e-7
-
-
-def test_matrix_to_gellmann_basis():
-    N0 = 5
-    for norm_I in ['1/d', 'sqrt(2/d)']:
-        np0 = hf_randc(N0,N0)
-        np1 = numpyqi.gellmann.matrix_to_gellmann_basis(np0, norm_I=norm_I)
-        np2 = numpyqi.gellmann.gellmann_basis_to_matrix(np1, norm_I=norm_I)
-        assert np.abs(np0-np2).max() < 1e-7
-
 
 def test_trace0_orthongal_matrix():
     dim = 4
