3c09cb11dc2cf176799f0b1f5ca7e0da23813c30
husisy <zchaozhang@outlook.com>
2023-06-21 16:47:50 +0800

    example/ add numerical range

--- example/demo_matrix_space_numerical_range.py
+++ example/demo_matrix_space_numerical_range.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numqi
 
 np_rng = np.random.default_rng()
+hf_randc = lambda *x: np_rng.normal(size=x) + 1j*np_rng.normal(size=x)
 
 
 def demo_matrix_numerical_range():
@@ -37,3 +38,55 @@ def demo_matrix_numerical_range():
     fig.tight_layout()
     fig.savefig('tbd00.png')
     # fig.savefig('data/demo_matrix_space_numerical_range.png')
+
+
+def demo_normal_matrix_numerical_range():
+    # NR(normal matrix) is the convex hull of eigenvalues
+    N0 = 4
+    matU = numqi.random.rand_unitary_matrix(N0)
+    EVL = hf_randc(N0)
+    matA = (matU * EVL) @ matU.T.conj()
+    boundary = numqi.matrix_space.get_matrix_numerical_range(matA, num_point=200)
+    fig,ax = plt.subplots()
+    ax.plot(boundary.real, boundary.imag)
+    ind0 = slice(None,None,5)
+    ax.plot(boundary.real[ind0], boundary.imag[ind0], '.', markersize=10)
+    ax.plot(EVL.real, EVL.imag, '+')
+    for ind0,x in enumerate(EVL):
+        ax.text(x.real, x.imag, str(ind0))
+    fig.tight_layout()
+    fig.savefig('tbd00.png', dpi=200)
+    plt.close(fig)
+
+def demo_abnormal_matrix_numerical_range():
+    # NR(abnormal matrix) is some mixture of the normal part and abnormal part
+    seed = np_rng.integers(0, 2**32-1)
+    np_rng = np.random.default_rng(seed=seed) #1457230309
+    hf_randc = lambda *x: np_rng.normal(size=x) + 1j*np_rng.normal(size=x)
+    N0 = 5
+    N1 = 2
+    matU = numqi.random.rand_unitary_matrix(N0, seed=np_rng)
+    EVL = hf_randc(N1)
+    matIN = hf_randc(N0-N1, N0-N1) #abnormal matrix
+    tmp0 = (matU[:,:N1]*EVL) @ matU[:,:N1].T.conj()
+    tmp1 = matU[:,N1:] @ matIN @ matU[:,N1:].T.conj()
+    matA0 = tmp0*0 + tmp1
+    matA1 = tmp0 + tmp1
+    boundary0 = numqi.matrix_space.get_matrix_numerical_range(matA0, num_point=200)
+    boundary1 = numqi.matrix_space.get_matrix_numerical_range(matA1, num_point=200)
+    # tmp0 = np.linalg.eigvalsh((matIN @ matIN.T.conj() - matIN.T.conj() @ matIN))
+
+    fig,ax = plt.subplots()
+    ax.plot(boundary0.real, boundary0.imag, label='NR(abnormal-part of $A$)')
+    ax.plot(boundary1.real, boundary1.imag, label='NR($A$)')
+    ind0 = slice(None,None)
+    ax.plot(boundary1.real[ind0], boundary1.imag[ind0], '.', markersize=10)
+    ax.plot(EVL.real, EVL.imag, '+', label='eigenvalue($A$)')
+    for ind0,x in enumerate(EVL):
+        ax.text(x.real, x.imag, str(ind0))
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig('tbd00.png', dpi=200)
+    plt.close(fig)
+
+# TODO demo joint algebraic numerical range
