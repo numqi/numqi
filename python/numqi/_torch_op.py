820856f23fba73f830c562d38c734694ae21abb6
husisy <zchaozhang@outlook.com>
2023-08-09 14:24:44 +0800

    bugfix, matrix-logarithm, divide zero

--- python/numqi/_torch_op.py
+++ python/numqi/_torch_op.py
@@ -21,40 +21,46 @@ class TorchMatrixSqrtm(torch.autograd.Function):
 
 
 def _torch_psd_sqrtm_forward_repeat(matA, repeat=1):
-    assert matA.ndim>=2
+    assert (matA.ndim==2) or (matA.ndim==3)
     repeat = int(repeat)
     assert repeat >= 1
-    tag_use_scipy = (not matA.is_cuda) and (matA.ndim==2)
-    if tag_use_scipy:
+    if (not matA.is_cuda) and (matA.ndim==2):
         ret = matA.numpy()
-        for _ in range(repeat):
-            ret = torch.from_numpy(scipy.linalg.sqrtm(ret).astype(ret.dtype))
-            # TODO .astype(matA.dtype) scipy-v1.10 bug https://github.com/scipy/scipy/issues/18250
+        if matA.ndim==2:
+            for _ in range(repeat):
+                # TODO .astype(matA.dtype) scipy-v1.10 bug https://github.com/scipy/scipy/issues/18250
+                ret = scipy.linalg.sqrtm(ret).astype(ret.dtype)
+        else: #TOO slow, not good (dead code)
+            for _ in range(repeat):
+                ret = [scipy.linalg.sqrtm(x).astype(x.dtype) for x in ret]
+            ret = np.stack(ret)
+        ret = torch.from_numpy(ret)
         ctx_tensor = ret,
     else:
         N0 = matA.shape[-1]
         shape = matA.shape[:-2]
         matA = matA.view(-1, N0, N0)
         EVL, EVC = torch.linalg.eigh(matA)
-        # tmp0 = torch.zeros(1, dtype=EVL.dtype, device=EVL.device)
         sqrt_EVL = torch.maximum(torch.zeros(1, dtype=EVL.dtype, device=EVL.device), EVL)
         for _ in range(repeat):
             sqrt_EVL = torch.sqrt(sqrt_EVL)
-        # sqrt_EVL = torch.sqrt(torch.maximum(tmp0, EVL))
         ret = ((EVC*sqrt_EVL.view(-1,1,N0)) @ EVC.transpose(1,2).conj()).view(*shape, N0, N0)
         ctx_tensor = sqrt_EVL, EVC
     return ret,ctx_tensor
 
 
 def _torch_psd_sqrtm_backward_repeat(grad_output, ctx_tensor, repeat=1):
-    assert grad_output.ndim>=2
+    assert (grad_output.ndim==2) or (grad_output.ndim==3)
     repeat = int(repeat)
     assert repeat>=1
     if (not grad_output.is_cuda) and (grad_output.ndim==2):
         tmp0 = ctx_tensor[0].numpy()
         ret = grad_output.numpy()
         for ind0 in range(repeat):
-            ret = scipy.linalg.solve_sylvester(tmp0, tmp0, ret)
+            if grad_output.ndim==2:
+                ret = scipy.linalg.solve_sylvester(tmp0, tmp0, ret)
+            else: #too slow, not good (dead code)
+                ret = np.stack([scipy.linalg.solve_sylvester(x, x, y) for x,y in zip(tmp0, ret)])
             if ind0!=repeat-1:
                 tmp0 = tmp0 @ tmp0
         ret = torch.from_numpy(ret)
@@ -65,9 +71,16 @@ def _torch_psd_sqrtm_backward_repeat(grad_output, ctx_tensor, repeat=1):
         sqrt_EVL,EVC = ctx_tensor
         EVCh = EVC.transpose(1,2).conj()
         ret = grad_output
+        if torch.any(sqrt_EVL==0).item():
+            ind_zero = torch.nonzero(sqrt_EVL==0)
+        else:
+            ind_zero = None
         for ind0 in range(repeat):
             tmp0 = sqrt_EVL.view(-1, 1, N0) + sqrt_EVL.view(-1, N0, 1)
-            ret = (EVC @ ((EVCh @ ret @ EVC) / tmp0) @ EVCh).view(*shape, N0, N0)
+            tmp1 = (EVCh @ ret @ EVC) / tmp0
+            if ind_zero is not None:
+                tmp1[ind_zero[:,0],ind_zero[:,1],ind_zero[:,1]] = 0
+            ret = (EVC @ tmp1 @ EVCh).view(*shape, N0, N0)
             if ind0!=repeat-1:
                 sqrt_EVL = sqrt_EVL**2
     return ret
