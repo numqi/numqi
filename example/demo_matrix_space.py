fbe367fcdc804b78536e9261c6210ae5e08efe47
husisy <zchaozhang@outlook.com>
2023-08-26 22:14:07 +0800

    example/ ignore

--- example/demo_matrix_space.py
+++ example/demo_matrix_space.py
@@ -71,36 +71,6 @@ def demo_matrix_space_UDA():
     print(f'loss(2): {theta_optim2.fun}, residual={residual}') #0
 
 
-
-def demo_hierarchical_XZ_over_real():
-    pauli_str = 'X Z'
-    matA,matB = numqi.gate.pauli_str_to_matrix(pauli_str, return_orth=True)
-    for k in range(1, 8):
-        tag,z0 = numqi.matrix_space.has_rank_hierarchical_method(matA, rank=2,
-                        hierarchy_k=k, use_real_field=True, return_space=True, num_worker=10)
-        print(k, tag, z0.shape, np.linalg.eigvalsh(z0@z0.T))
-    # 1, False (3, 2) [0. 0. 2.]
-    # 2 False (4, 8) [0 0  2.22222222e+00  2.22222222e+00]
-    # 3 False (5, 32) [0  0  1.000e+00  4.000e+00  4.222e+00]
-    # 4 False (6, 128) [0  0  1.197e+00  1.197e+00  8.083e+00  8.083e+00]
-    # 5 False (7, 512) [0  0  8.493e-01  1.778e+00  2.098e+00  1.607e+01  1.608e+01]
-    # 6 False (8, 2048) [0 0 9.711e-01 9.711e-01 3.410e+00 3.410e+00 3.207e+01 3.207e+01]
-    # 7 False (9, 8192) [0 0 7.976e-01 1.223e+00 1.615e+00 6.122e+00 6.141e+00 6.408e+01 6.408e+01]
-
-    pauli_str = 'XX XY'
-    matA,matB = numqi.gate.pauli_str_to_matrix(pauli_str, return_orth=True)
-    for k in range(1,7):
-        tag,z0 = numqi.matrix_space.has_rank_hierarchical_method(matA, rank=4,
-                        hierarchy_k=k, use_real_field=True, return_space=True, num_worker=10)
-        print(k, tag, z0.shape, np.linalg.eigvalsh(z0@z0.T))
-    # 1 False [-2.681e-16  0.000e+00  1.844e-17  7.976e-17  2.111e+00]
-    # 2 False [-1.233e-16 -3.619e-18  6.405e-17  4.757e-16  4.320e+00  4.320e+00]
-    # 3 False [-2.211e-15 -1.636e-16 -1.182e-16  1.991e-16  2.098e+00  1.607e+01  1.664e+01]
-    # 4 False (8,8192) [-8.531e-15 -2.173e-15  4.899e-16  9.227e-16  4.973e+00  4.973e+00  6.464e+01  6.464e+01]
-    # 5 False [-3.004e-14 -5.318e-16  3.281e-15  1.230e-14  5.145e+00  1.633e+01  1.894e+01  2.573e+02  2.574e+02]
-    # 6 False [-4.415e-14 -3.072e-14  1.263e-13  1.504e-13  1.174e+01  1.174e+01  6.660e+01  6.660e+01  1.027e+03  1.027e+03]
-
-
 def demo_hierarchical_method_random_matrix_subspace():
     dimA = 4
     dimB = 4
