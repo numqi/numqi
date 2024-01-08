2e04ea962a2841b40632d776532f72644a60d192
husisy <zchaozhang@outlook.com>
2024-01-08 14:40:37 +0800

    project/entangler cleanup

--- project/entangler/draft00.py
+++ project/entangler/draft00.py
@@ -22,17 +22,13 @@ class DummyModel(torch.nn.Module):
         assert (matU.ndim==2) and (matU.shape[0]==dimA*dimB) and (matU.shape[0]==matU.shape[1])
         assert np.abs(matU @ matU.T.conj() - np.eye(matU.shape[0])).max() < 1e-10
         self.loss = loss
-        np_rng = np.random.default_rng()
-        hf0 = lambda *x: torch.nn.Parameter(torch.tensor(np_rng.uniform(-1,1,size=x), dtype=torch.float64))
-        self.thetaA = hf0(2, dimA)
-        self.thetaB = hf0(2, dimB)
+        self.manifold_psiA = numqi.manifold.Sphere(dimA, dtype=torch.complex128)
+        self.manifold_psiB = numqi.manifold.Sphere(dimB, dtype=torch.complex128)
         self.matU = torch.tensor(matU, dtype=torch.complex128)
 
     def forward(self):
-        tmp0 = self.thetaA / torch.linalg.norm(self.thetaA)
-        stateA = torch.complex(tmp0[0], tmp0[1])
-        tmp0 = self.thetaB / torch.linalg.norm(self.thetaB)
-        stateB = torch.complex(tmp0[0], tmp0[1])
+        stateA = self.manifold_psiA()
+        stateB = self.manifold_psiB()
         state_out = (self.matU.reshape(-1, stateB.shape[0]) @ stateB).reshape(-1, stateA.shape[0]) @ stateA
         tmp0 = state_out.reshape(stateA.shape[0], stateB.shape[0])
         if self.loss=='eigen':
@@ -42,8 +38,6 @@ class DummyModel(torch.nn.Module):
             tmp0 = (rdmA - rdmA @ rdmA).reshape(-1)
             loss = torch.vdot(tmp0, tmp0).real
         return loss
-
-
 
 
 def demo00():
@@ -104,24 +98,25 @@ if __name__=='__main__':
     #         print(tmp0)
     #         break
     # to_pickle(theta_u=theta_u)
-    theta_u = from_pickle('theta_optim_u')
 
-    # hf0 = lambda x, tag_grad=True: hf_dummy_233(x, tag_grad, kwargs_model, kwargs_optim, zero_eps=1e-9, num_worker=16)
-    # hf_callback = numqi.optimize.MinimizeCallback(print_freq=1).to_callable(hf0) #extra_key='path'
-    # theta_optim = scipy.optimize.minimize(hf0, theta_u, method='L-BFGS-B', jac=True, callback=hf_callback)
-
-    hf0 = lambda x, tag_grad=False: hf_dummy_233(x, tag_grad, kwargs_model, kwargs_optim, zero_eps=1e-7, num_worker=16)
-    hf_callback = numqi.optimize.MinimizeCallback(print_freq=5).to_callable(hf0) #extra_key='path'
-    theta_optim = scipy.optimize.minimize(hf0, theta_u, method='Nelder-Mead', callback=hf_callback)
-    from zzz233 import to_pickle
-    to_pickle(theta_optim_u=theta_optim.x)
+    num_round = 1000
+    t0 = time.time()
+    for ind_round in range(num_round):
+        theta_u = from_pickle('theta_optim_u')
+        hf0 = lambda x, tag_grad=False: hf_dummy_233(x, tag_grad, kwargs_model, kwargs_optim, zero_eps=1e-7, num_worker=16)
+        hf_callback = numqi.optimize.MinimizeCallback(print_freq=50).to_callable(hf0) #extra_key='path'
+        theta_optim = scipy.optimize.minimize(hf0, theta_u, method='Nelder-Mead', callback=hf_callback)
+        total_time = time.time() - t0
+        average_time = total_time / (ind_round+1)
+        eta_time = total_time*num_round / (ind_round+1) - total_time
+        to_pickle(theta_optim_u=theta_optim.x)
+        print(f'[{ind_round+1}/{num_round}][{average_time:.0f}s / {eta_time:.0f}s / {total_time:.0f}s] loss={theta_optim.fun:.6f}')
 
     # theta_optim_u = from_pickle('theta_optim_u')
     # matU = numqi.param.real_matrix_to_special_unitary(theta_optim_u.reshape(dimA*dimB, dimA*dimB))
     # model = DummyModel(matU, **kwargs_model)
     # fval = -numqi.optimize.minimize(model, **kwargs_optim).fun
     # model.loss = 'eigen'
-
     # hf_dummy_233(theta_optim_u, True, dict(dim=(dimA,dimB), loss='eigen'), kwargs_optim, zero_eps=1e-9, num_worker=16)
 
 # https://www.sciencedirect.com/science/article/pii/S037596011401216X
--- project/entangler/draft01.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-import subprocess
-
-
-
-for ind0 in range(100):
-    subprocess.run(['python', '-u', 'draft00.py']) #block until subprocess finish
-    shutil.copy('tbd00.pkl', f'data/tbd00_{ind0}.pkl')
