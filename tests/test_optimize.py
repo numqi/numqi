ab378c3fa4d7ccaf701eed6591be3acf74008480
husisy <17511292+husisy@users.noreply.github.com>
2024-10-15 20:47:28 +0800

    numqi.optimize.minimize/ more example for constrained optimization

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -44,3 +44,78 @@ def test_optimize_minimize_with_constraint():
     loss,constraint = model()
     assert abs(loss.item()-0.5) < 1e-5 #about 1e-7
     assert constraint.item()**2 < 1e-14
+
+
+# https://en.wikipedia.org/wiki/Test_functions_for_optimization
+class RosenBrockCubicLineModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.manifold_x = numqi.manifold.OpenInterval(-1.5, 1.5, dtype=torch.float64)
+        self.manifold_y = numqi.manifold.OpenInterval(-0.5, 2.5, dtype=torch.float64)
+        self.manifold_positive = numqi.manifold.PositiveReal(batch_size=2, method='softplus', dtype=torch.float64)
+        # f(1,1)=0
+
+    def forward(self):
+        x = self.manifold_x()
+        y = self.manifold_y()
+        loss = (1-x)**2 + 100*(y-x**2)**2
+        p_positive = self.manifold_positive()
+        constraint = torch.stack([(x-1)**3-y+1+p_positive[0], x+y-2+p_positive[1]])
+        return loss, constraint
+
+def test_RosenBrockCubicLineModel():
+    model = RosenBrockCubicLineModel()
+    theta_optim = numqi.optimize.minimize(model, num_repeat=5, tol=1e-15, method='L-BFGS-B',
+                    constraint_penalty=10, constraint_p=1.6, constraint_threshold=1e-14)
+    loss,constraint = model()
+    assert abs(loss.item()) < 1e-5 #about 1e-7
+    assert np.abs(constraint.detach().numpy()).max()**2 < 1e-14
+
+
+class RosenBrockDisk(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.manifold_x = numqi.manifold.OpenInterval(-1.5, 1.5, dtype=torch.float64)
+        self.manifold_y = numqi.manifold.OpenInterval(-1.5, 1.5, dtype=torch.float64)
+        self.manifold_positive = numqi.manifold.PositiveReal(method='softplus', dtype=torch.float64)
+        # f(1,1)=0
+
+    def forward(self):
+        x = self.manifold_x()
+        y = self.manifold_y()
+        loss = (1-x)**2 + 100*(y-x**2)**2
+        constraint = x*x + y*y - 2 + self.manifold_positive()
+        return loss, constraint
+
+def test_RosenBrockDisk():
+    model = RosenBrockDisk()
+    theta_optim = numqi.optimize.minimize(model, num_repeat=5, tol=1e-15, method='L-BFGS-B',
+                    constraint_penalty=10, constraint_p=1.6, constraint_threshold=1e-14)
+    loss,constraint = model()
+    assert abs(loss.item()) < 1e-5 #about 1e-7
+    assert constraint.item()**2 < 1e-14
+
+
+class MishraBirdModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.manifold = numqi.manifold.Ball(2, dtype=torch.float64)
+        self.manifold_positive = numqi.manifold.PositiveReal(method='softplus', dtype=torch.float64)
+        # f(-3.1302468,-1.5821422)=-106.7645367
+
+    def forward(self):
+        x,y = self.manifold() * 5 - 5
+        cx = torch.cos(x)
+        sy = torch.sin(y)
+        loss = sy * torch.exp((1-cx)**2) + cx * torch.exp((1-sy)**2) + (x-y)**2
+        constraint = y + 6.5 - self.manifold_positive()
+        return loss, constraint
+
+def test_MishraBirdModel():
+    model = MishraBirdModel()
+    # need large repeats to converge with high probability
+    theta_optim = numqi.optimize.minimize(model, num_repeat=100, tol=1e-15, method='L-BFGS-B',
+                    constraint_penalty=10, constraint_p=1.1, constraint_threshold=1e-14)
+    loss,constraint = model()
+    assert abs(loss.item()+106.7645367) < 1e-5 #about 1e-7
+    assert constraint.item()**2 < 1e-14
