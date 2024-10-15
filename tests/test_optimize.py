cb57637a5f6c4fb38e4c0ec32a48ef85722d60d1
husisy <17511292+husisy@users.noreply.github.com>
2024-10-15 16:57:42 +0800

    numqi.optimize/minimize(experimental feature) add unconstrained opt

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -20,3 +20,27 @@ class Rosenbrock(torch.nn.Module):
 def test_gradient_correct():
     model = Rosenbrock(num_parameter=5)
     numqi.optimize.check_model_gradient(model, zero_eps=1e-4)
+
+
+class DummyModel02(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        np_rng = np.random.default_rng()
+        hf0 = lambda *x: torch.nn.Parameter(torch.tensor(np_rng.uniform(-1,1,size=x), dtype=torch.float64))
+        self.theta = hf0(2)
+        self.manifold_positive = numqi.manifold.PositiveReal(method='softplus', dtype=torch.float64)
+
+    def forward(self):
+        loss = self.theta[0]**2 + self.theta[1]**2
+        p_positive = self.manifold_positive()
+        constraint = self.theta[0] - self.theta[1] - 1 - p_positive
+        return loss,constraint
+
+
+def test_optimize_minimize_with_constraint():
+    model = DummyModel02()
+    theta_optim = numqi.optimize.minimize(model, num_repeat=5, tol=1e-15, method='L-BFGS-B',
+                    constraint_penalty=10, constraint_p=1.1, constraint_threshold=1e-14)
+    loss,constraint = model()
+    assert abs(loss.item()-0.5) < 1e-5 #about 1e-7
+    assert constraint.item()**2 < 1e-14
