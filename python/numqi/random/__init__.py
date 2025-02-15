cb33ae5a890d8a20deecc3c776f2623ccf253b11
husisy <17511292+husisy@users.noreply.github.com>
2025-02-15 16:03:27 +0800

    doc/api add more numqi.random

--- python/numqi/random/__init__.py
+++ python/numqi/random/__init__.py
@@ -5,7 +5,8 @@ from ._internal import (rand_haar_state, rand_haar_unitary,
             rand_bipartite_state, rand_separable_dm, rand_hermitian_matrix, rand_channel_matrix_space,
             rand_quantum_channel_matrix_subspace, rand_ABk_density_matrix, rand_reducible_matrix_subspace,
             rand_symmetric_inner_product, rand_orthonormal_matrix_basis,
-            rand_adjacent_matrix, rand_povm, rand_n_sphere, rand_n_ball, rand_Stiefel_matrix)
+            rand_adjacent_matrix, rand_povm, rand_n_sphere, rand_n_ball, rand_Stiefel_matrix,
+            rand_discrete_probability)
 
 from ._spf2 import rand_F2, rand_SpF2, rand_Clifford_group, rand_pauli
 
